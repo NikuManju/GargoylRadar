7281a5184bd0ff6a8bab5eaa4753f3f9874e6c0e
Nils <nils.hausner@gmx.de>
2022-03-13 21:02:29 +0100

    made canvas "reset" and surfaceDestroyed-event handled. TODO adding function for spawning points on radar

--- app/src/main/java/Mythen/gargoylRadarFinal/GameView.java
+++ app/src/main/java/Mythen/gargoylRadarFinal/GameView.java
@@ -3,22 +3,29 @@ package Mythen.gargoylRadarFinal;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.motion.widget.Debug;
 
-public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+public class GameView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
 
     private Paint p;
     private SurfaceHolder holder;
+    private Handler handler;
+    private int angle;
+
     public GameView(Context context) {
         super(context);
         initView();
@@ -27,15 +34,22 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
+        canvas.drawColor(Color.BLACK);
         int r = 30;
         for(int i = 0; i < 5; i++){
             canvas.drawCircle(getWidth()/2,getHeight()/2,r,p);
             r+=100;
         }
+        angle+=10;
+        canvas.drawLine(getWidth()/2, getHeight()/2,
+                (float) (getWidth()/2-(r*Math.cos(Math.toRadians(angle)))),
+                (float)(getHeight()/2-(r*Math.sin(Math.toRadians(angle)))), p);
     }
 
     private void initView(){
         holder = getHolder();
+        handler = new Handler();
+        angle = 0;
         holder.addCallback(this);
         setFocusable(true);
         SetPaint();
@@ -52,9 +66,8 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     @SuppressLint("WrongCall")
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder) {
-        Canvas can = holder.lockCanvas();
-        onDraw(can);
-        holder.unlockCanvasAndPost(can);
+        handler.postDelayed(this,500);
+
     }
 
     @Override
@@ -64,6 +77,18 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    @SuppressLint("WrongCall")
+    @Override
+    public void run() {
+        synchronized (handler){
+            Canvas can = holder.lockCanvas();
+            onDraw(can);
+            holder.unlockCanvasAndPost(can);
+            handler.postDelayed(this,100);
+        }
 
     }
 }
